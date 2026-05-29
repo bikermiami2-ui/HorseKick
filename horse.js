e2a448bcdb3b7f718b7295f729f7b5bdb8e199d1
Ap <bikermiami2@gmail.com>
2026-05-30 02:23:59 +0300

    feat: add cowboy hat to horse

--- horse.js
+++ horse.js
@@ -19,11 +19,13 @@ class Horse {
         const kick = this.kickAnim;
         const gy = this.groundY;
         ctx.save(); ctx.translate(this.x, gy);
+
         // body
         ctx.fillStyle = '#8B6914';
         ctx.beginPath();
         ctx.ellipse(0, -40*s + breathe, 40*s, 25*s, 0, 0, Math.PI*2);
         ctx.fill();
+
         // neck + head
         ctx.fillStyle = '#7A5C12';
         ctx.beginPath();
@@ -32,6 +34,31 @@ class Horse {
         ctx.quadraticCurveTo(-20*s, -105*s + breathe, -15*s, -90*s + breathe);
         ctx.quadraticCurveTo(-10*s, -60*s + breathe, -15*s, -50*s + breathe);
         ctx.fill();
+
+        // === COWBOY HAT ===
+        const hatY = -103*s + breathe;
+        const hatX = -28*s;
+        // brim
+        ctx.fillStyle = '#3a2510';
+        ctx.beginPath();
+        ctx.ellipse(hatX, hatY, 22*s, 6*s, -0.1, 0, Math.PI*2);
+        ctx.fill();
+        // crown
+        ctx.fillStyle = '#4a3018';
+        ctx.beginPath();
+        ctx.moveTo(hatX - 12*s, hatY);
+        ctx.quadraticCurveTo(hatX - 13*s, hatY - 18*s, hatX - 5*s, hatY - 20*s);
+        ctx.quadraticCurveTo(hatX, hatY - 16*s, hatX + 5*s, hatY - 20*s);
+        ctx.quadraticCurveTo(hatX + 13*s, hatY - 18*s, hatX + 12*s, hatY);
+        ctx.fill();
+        // hat band
+        ctx.strokeStyle = '#c8a040';
+        ctx.lineWidth = 2*s;
+        ctx.beginPath();
+        ctx.moveTo(hatX - 11*s, hatY - 5*s);
+        ctx.quadraticCurveTo(hatX, hatY - 3*s, hatX + 11*s, hatY - 5*s);
+        ctx.stroke();
+
         // ear
         ctx.fillStyle = '#6B4F10';
         ctx.beginPath();
@@ -39,25 +66,28 @@ class Horse {
         ctx.lineTo(-38*s, -115*s + breathe);
         ctx.lineTo(-26*s, -103*s + breathe);
         ctx.fill();
+
         // eye
         ctx.fillStyle = '#fff';
         ctx.beginPath(); ctx.arc(-30*s, -95*s + breathe, 3*s, 0, Math.PI*2); ctx.fill();
         ctx.fillStyle = '#000';
         ctx.beginPath(); ctx.arc(-30*s, -95*s + breathe, 1.5*s, 0, Math.PI*2); ctx.fill();
+
         // front legs
         ctx.strokeStyle = '#7A5C12'; ctx.lineWidth = 6*s; ctx.lineCap = 'round';
         ctx.beginPath(); ctx.moveTo(-20*s, -20*s + breathe); ctx.lineTo(-22*s, 0); ctx.stroke();
         ctx.beginPath(); ctx.moveTo(-10*s, -20*s + breathe); ctx.lineTo(-8*s, 0); ctx.stroke();
+
         // back legs with kick animation
         const kickAngle = kick * 1.2;
         ctx.save();
         ctx.translate(25*s, -20*s + breathe);
         ctx.rotate(-kickAngle);
         ctx.beginPath(); ctx.moveTo(0, 0); ctx.lineTo(5*s, 25*s); ctx.stroke();
-        // hoof
         ctx.fillStyle = '#333';
         ctx.beginPath(); ctx.arc(5*s, 25*s, 4*s, 0, Math.PI*2); ctx.fill();
         ctx.restore();
+
         ctx.save();
         ctx.translate(30*s, -20*s + breathe);
         ctx.rotate(-kickAngle * 0.8);
@@ -65,6 +95,7 @@ class Horse {
         ctx.fillStyle = '#333';
         ctx.beginPath(); ctx.arc(3*s, 25*s, 4*s, 0, Math.PI*2); ctx.fill();
         ctx.restore();
+
         // tail
         ctx.strokeStyle = '#5a4010'; ctx.lineWidth = 3*s;
         const tailWag = Math.sin(this.idleAnim * 2) * 0.3;
@@ -72,6 +103,7 @@ class Horse {
         ctx.moveTo(38*s, -45*s + breathe);
         ctx.quadraticCurveTo(55*s, -50*s + breathe + tailWag*10, 50*s, -30*s + breathe);
         ctx.stroke();
+
         // mane
         ctx.strokeStyle = '#5a4010'; ctx.lineWidth = 2*s;
         for (let i = 0; i < 5; i++) {
